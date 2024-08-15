e8ccd0090e4a601ac89bfb9c47af8e2033a62801
ThiagoDev202 <thiago2002.fernandes@hotmail.com>
2024-08-15 20:03:16 -0300

    feat: schema setup

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './schemas/product.schema';
+
 
 @Module({
-  providers: [ProductService],
-  controllers: [ProductController]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }])
+  ],
+  controllers: [ProductController],
+  providers: [ProductService]
 })
 export class ProductModule {}
