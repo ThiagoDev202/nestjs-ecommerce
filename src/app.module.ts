a9dc144df5f71278bb042428efc03f6549c7e282
ThiagoDev202 <thiago2002.fernandes@hotmail.com>
2024-08-17 18:35:44 -0300

    feat: user service DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     // db setup
     MongooseModule.forRoot('mongodb://localhost/store'),
     ProductModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserSchema } from './user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([ { name: 'User', schema: UserSchema } ])
+  ],
+  providers: [UserService],
+  exports: [UserService]
+})
+
+export class UserModule { }
