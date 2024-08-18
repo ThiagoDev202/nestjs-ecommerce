2e4a0e5a585594a07c378168fa05de5f5f98844f
ThiagoDev202 <thiago2002.fernandes@hotmail.com>
2024-08-17 21:14:36 -0300

    feat: endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Request, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateUserDTO } from 'src/user/dtos/create-user.dto';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local.guard';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { Roles } from './decorators/roles.decorator';
+import { Role } from './guards/enums/role.enum';
+import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService, private userService: UserService) {}
+
+  // endpoints
+
+  // create new user
+  @Post('/register')
+  async register(@Body() createUserDTO: CreateUserDTO) {
+    const user = await this.userService.addUser(createUserDTO);
+    return user;
+  }
+
+  // log in a registered user
+  @UseGuards(LocalAuthGuard) // verify the user
+  @Post('/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  // access the user's profile
+  @UseGuards(JwtAuthGuard, RolesGuard) // authenticate and authorization
+  @Roles(Role.User)
+  @Get('/user')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  // acess the admin dashboard
+  @UseGuards(JwtAuthGuard, RolesGuard) // authenticate and authorization
+  @Roles(Role.Admin)
+  @Get('/admin')
+  getDashboard(@Request() req) {
+    return req.user;
+  }
+}
